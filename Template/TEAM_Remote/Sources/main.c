1169018a8c20758db81b126c5bc23d6e200a07d5
IvoBissig <43415683+IvoBissig@users.noreply.github.com>
2018-09-21 16:04:09 +0200

    added a for-loop

--- Template/TEAM_Remote/Sources/main.c
+++ Template/TEAM_Remote/Sources/main.c
@@ -92,7 +92,7 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   APP_Start();
-
+  for(;;){}
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
